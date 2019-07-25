3e2c5ebf007806258f07ed3c3a5fe952bd5b1c30
Umesh Kumar <mrumeshkumar@hotmail.com>
2019-07-25 20:21:46 +0530

    adding jenkins file

---
+++ target/generated-sources/groovy-stubs/test/DefaultPipelineSpec.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class DefaultPipelineSpec
+  extends com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification {
+;
+public  java.lang.Object getDefaultPipeline() { return null;}
+public  void setDefaultPipeline(java.lang.Object value) { }
+public  java.lang.Object setup() { return null;}
+public static class DummyException
+  extends java.lang.RuntimeException  implements
+    groovy.lang.GroovyObject {
+;
+public DummyException
+(java.lang.String _message) {
+super ((java.lang.String)null, (java.lang.Throwable)null, false, false);
+}
+public  groovy.lang.MetaClass getMetaClass() { return (groovy.lang.MetaClass)null;}
+public  void setMetaClass(groovy.lang.MetaClass mc) { }
+public  java.lang.Object invokeMethod(java.lang.String method, java.lang.Object arguments) { return null;}
+public  java.lang.Object getProperty(java.lang.String property) { return null;}
+public  void setProperty(java.lang.String property, java.lang.Object value) { }
+}
+}
---
+++ target/generated-sources/groovy-stubs/test/DeployerSpec.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import groovy.lang.*;
+import groovy.util.*;
+
+public class DeployerSpec
+  extends com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification {
+;
+public  java.lang.Object getDeployer() { return null;}
+public  void setDeployer(java.lang.Object value) { }
+public  java.lang.Object setup() { return null;}
+}
